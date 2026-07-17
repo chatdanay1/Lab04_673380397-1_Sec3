ea74305874f80f91c979533c3dc45d90ae89059e
suphawat1 <suphawatcccc@gmail.com>
2026-07-17 22:18:16 +0700

    ใส่คำนำหน้าชื่อ เป็นคำว่า นาย

--- src/main/java/com/example/lab04/controller/HelloController.java
+++ src/main/java/com/example/lab04/controller/HelloController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     // ── ข้อมูลนักศึกษา ── แก้ไขตรงนี้ ──────────────────────────────────────
-    private static final String NAME    = "ศุภวัทน์ แสนเรียน";     // TODO: ใส่ชื่อจริง
+    private static final String NAME    = "นายศุภวัทน์ แสนเรียน";     // TODO: ใส่ชื่อจริง
     private static final String ID      = "673380604-2";               // TODO: ใส่รหัสนักศึกษา
     private static final String SECTION = "3";                  // TODO: ใส่เลข Section
     // ─────────────────────────────────────────────────────────────────────────
